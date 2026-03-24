6320976499dfdcd742db43a5382c94929784fd41
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-03-25 03:34:50 +0600

    [2167B | 800] Accepted | Time: 15ms | Memory: 100KB

---
+++ 800/2167B/2167B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+
+  int n;
+  cin>>n;
+  string s ,t;
+  cin>>s>>t;
+  sort(all(s));
+  sort(all(t));
+  if (s==t)cout<<"YES\n";
+  else cout<<"NO\n";
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}

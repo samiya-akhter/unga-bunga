5d6af65158dd870e97ed70b6dc64910155ab4f13
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-03-25 03:31:03 +0600

    [2167A | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ 800/2167A/2167A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  if(a==b&&b==c&&c==d&&d==a) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}

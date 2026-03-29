5070b634788a01077f09f803fb8c9d0fea7b2868
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-03-29 20:52:04 +0600

    [2210A | unrated] Accepted | Time: 46ms | Memory: 100KB

---
+++ Unrated/2210A/2210A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+
+
+  int n;
+  cin>>n;
+  for(int i=n;i>0;i--){
+    cout<<i<<" ";
+  }
+  cout<<endl;
+
+
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}

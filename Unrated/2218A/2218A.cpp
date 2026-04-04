9ae43bb5302684738fdf70293dc742472b53ff14
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-04-04 21:24:01 +0600

    [2218A | unrated] Accepted | Time: 31ms | Memory: 0KB

---
+++ Unrated/2218A/2218A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  int n;
+  cin>>n;
+  
+  cout<<67<<endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
